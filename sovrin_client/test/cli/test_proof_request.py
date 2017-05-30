65dce9ca56d5a339305e43bf614f891879b64ac8
Devin Fisher <devin.fisher@evernym.com>
2017-05-30 12:24:17 -0600

    removed merge artifacts

--- sovrin_client/test/cli/test_proof_request.py
+++ sovrin_client/test/cli/test_proof_request.py
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream
-import pytest
-
-
-@pytest.mark.skip('INDY-85. Not implemented yet')
-=======
->>>>>>> Stashed changes
 def test_show_nonexistant_proof_request(be, do, aliceCLI):
     be(aliceCLI)
     do("show proof request Transcript", expect=["No matching Proof Requests found in current keyring"], within=1)
